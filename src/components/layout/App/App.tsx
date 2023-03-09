f04de434f05810d8e07ddb28db92f41af99df1bf
Fpsska <fpsska1337@gmail.com>
2023-03-09 13:37:49 +0300

    feat: create page__controls HMTL-markup, ModeSwitcher.tsx component

--- src/components/layout/App/App.tsx
+++ src/components/layout/App/App.tsx
@@ -6,6 +6,8 @@ import Button from 'components/ui/Button/Button';
 
 import Placeholder from 'components/ui/Placeholder/Placeholder';
 
+import ModeSwitcher from 'components/ui/ModeSwitcher/ModeSwitcher';
+
 import Section from '../Section/Section';
 
 import { operators, numbers } from '../../../context/db';
@@ -19,31 +21,36 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <div className="page">
-                <div className="page__preview">
-                    <div className="storage">
-                        <div className="storage__wrapper">
-                            <Section role="section_display">
-                                <Display />
-                            </Section>
-                            <Section
-                                role="section_operators"
-                                data={operators}
-                            />
-                            <Section
-                                role="section_numbers"
-                                data={numbers}
-                            />
-                            <Section role="section_others">
-                                <Button
-                                    role="button_equal"
-                                    symbol="="
-                                />
-                            </Section>
-                        </div>
+                <div className="page__wrapper">
+                    <div className="page__controls">
+                        <ModeSwitcher />
                     </div>
-                    <div className="constructor empty">
-                        <div className="constructor__wrapper">
-                            <Placeholder />
+                    <div className="page__preview">
+                        <div className="page__storage storage">
+                            <div className="storage__wrapper">
+                                <Section role="section_display">
+                                    <Display />
+                                </Section>
+                                <Section
+                                    role="section_operators"
+                                    data={operators}
+                                />
+                                <Section
+                                    role="section_numbers"
+                                    data={numbers}
+                                />
+                                <Section role="section_others">
+                                    <Button
+                                        role="button_equal"
+                                        symbol="="
+                                    />
+                                </Section>
+                            </div>
+                        </div>
+                        <div className="constructor empty">
+                            <div className="constructor__wrapper">
+                                <Placeholder />
+                            </div>
                         </div>
                     </div>
                 </div>
